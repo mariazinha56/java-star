302cf566dbb20d24e0913128700db3ed6fd0ce90
mariazinha56 <143208855+mariazinha56@users.noreply.github.com>
2024-09-23 10:18:21 -0300

    jyyy

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function soma(num1,num2){
 }
 
 console.log(soma(2,3));
+
+function areaQuadro(lado){
+    return lado **2
+}
+console.log(areaquadrado(3));
+function areatriangulo(base,altura){
+    return base*altura/2
+}
+console.log(areatriangulo(8,5))
