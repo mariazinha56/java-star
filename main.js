328e2a60ed2ecc4d35b582f6d6e3d35aeea39428
mariazinha56 <143208855+mariazinha56@users.noreply.github.com>
2024-09-23 10:19:34 -0300

    c b

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function areaQuadro(lado){
     return lado **2
 }
 console.log(areaquadrado(3));
-function areatriangulo(base,altura){
-    return base*altura/2
+
+function areatriangulo(base,altura,){
+    return base*altura/2;
 }
 console.log(areatriangulo(8,5))
